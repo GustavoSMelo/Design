9b24403091550259b1c1a810bb25ee6e2126fb63
Gustavo S. Melo <gsantos15569@gmail.com>
2020-11-11 01:15:39 -0300

    add navbar in project

--- layout01/components/navbar/styled.ts
+++ layout01/components/navbar/styled.ts
@@ -1,8 +1,28 @@
 import styled from 'styled-components'
 
-export const Navbar = styled.header`
+export const Header = styled.header<{ textcolor: string}>`
     display: flex;
     flex-direction: row;
     position: fixed;
     top: 0;
+    justify-content: space-between;
+    color: ${props => props.textcolor};
+    background-color: transparent;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    width: 100vw;
+    padding: 30px;
+
+    nav {
+        display: flex;
+    }
+
+    nav > h1 {
+        margin: 15px;
+        cursor: pointer;
+    }
+
+    article > span {
+        margin: 10px;
+        cursor: pointer;
+    }
 `
